f9f9d211a76e74ac312c42b9ebb914bdfd6ab463
Piotr Niewinski <me@piotrniewinski.com>
2021-10-01 07:10:42 +0200

    use_more

--- model_hpmser.py
+++ model_hpmser.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
         #('use_one_hidden',      10),
         #('use_hidden_stack',    10),
         #('use_drt',             10),
-        ('use_more',            10),
+        ('use_more',            6),
         #('seq_reduced',         10),
         #('seq_cnn',             2),
         #('seq_tns',             4),
